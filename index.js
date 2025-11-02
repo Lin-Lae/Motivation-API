ea9e8e25cfeb928c47be6ccfbc621ca33273f5aa
Lin <lynnlae22@gmail.com>
2025-11-02 15:27:42 +0530

    Added ejs and dynamic port for Render deployment

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import ejs from "ejs";
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
-
-// app.get("/", (req, res) => {
-//   res.render("index.ejs");
-// });
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   const index = Math.floor(Math.random() * quotes.length);
